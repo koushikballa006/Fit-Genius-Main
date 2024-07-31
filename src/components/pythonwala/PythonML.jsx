721cd14a6418c05698ae77ae04f2aaa14173ec10
koushikballa006 <koushikballa06@gmail.com>
2024-07-31 10:00:03 +0530

    Initial commit

---
+++ src/components/pythonwala/PythonML.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PythonML = () => {
+  return (
+    <iframe
+        src="https://fitgenius-ai-1.onrender.com"
+        title="Streamlit App"
+        // style={{ width: '100%', height: '600px', border: 'none' }}
+        className='w-[100%] h-[85%]'
+    />
+);
+}
+
+export default PythonML
